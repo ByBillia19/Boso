c1426a2a31d328ddc160a984410e66e6e2c626fa
ByBillia19 <KEAMAFORA19@GMAIL.COM>
2024-02-10 18:39:24 +0200

    called search

--- src/index.js
+++ src/index.js
@@ -24,3 +24,4 @@ searchCity(searchInput.value);
 }
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+searchCity("Polokwane");

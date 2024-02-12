32fa8a84fde09da759859a0604f7cf52bb1983a3
ByBillia19 <KEAMAFORA19@GMAIL.COM>
2024-02-12 22:21:22 +0200

    added condition,description and wind speed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ function refreshWeather (response){
     let temparatureElement=document.querySelector("#temparature");
     let temparature =response.data.temparature.curent;
     temparatureElement.innerHTML=Math.round(temparature);
+    let discriptionElement=document.querySelector("#discription");
+   let humidityElement=document.querySelector("#humidity");
+   let windSpeedElement=document.querySelector("#wind-speed");
+    
 
     let cityElement=document.querySelector("#city");
+    discriptionElement.innerHTML=response.data.condition.discription;
     cityElement.innerHTML=response.data.city;
+    humidityElement.innerHTML='${response.data.temparature.humidity}%';
+    temparatureElement.innerHTML=Math.round(temparature);
+    windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
 
 }
 
 function searchCity (city){
-    let apiKey="bof6afef739a369a2ca570a747e3a1ct";
+    let apiKey='bof6afef739a369a2ca570a747e3a1ct';
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(refreshWeather);
 }

e8a4033e2136ea7322cbe92fd26829173edfe46e
ByBillia19 <KEAMAFORA19@GMAIL.COM>
2024-02-13 22:49:53 +0200

    redid js file

--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
-function refreshWeather (response){
-    console.log(temparature)
-    let temparatureElement=document.querySelector("#temparature");
-    let temparature =response.data.temparature.curent;
-    temparatureElement.innerHTML=Math.round(temparature);
-    let discriptionElement=document.querySelector("#discription");
-   let humidityElement=document.querySelector("#humidity");
-   let windSpeedElement=document.querySelector("#wind-speed");
-    let timeElement=document.querySelector("#time");
-    let date=new Date(response.data.time * 1000);
-    
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-    let cityElement=document.querySelector("#city");
-    discriptionElement.innerHTML=response.data.condition.discription;
-    cityElement.innerHTML=response.data.city;
-    humidityElement.innerHTML='${response.data.temparature.humidity}%';
-    temparatureElement.innerHTML=Math.round(temparature);
-    windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
-    timeElement.innerHTML=fomatDate(date);
-
-}
-function fomatDate (date) {
-    let minutes=date.getMinutes();
-    let hour=date.getHours();
-    let days= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
-let day= days[date.getDay()];
-if (minutes >10){
-    minutes='0${minutes}';
-}
-return '${days} ${hour}:${minutes}';
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
-function searchCity (city){
-    let apiKey='bof6afef739a369a2ca570a747e3a1ct';
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-axios.get(apiUrl).then(refreshWeather);
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-function handleSearchSubmit (event){
-    event.preventDefault();
-    let searchInput= document.querySelector("#search-form-input");
-
-
-searchCity(searchInput.value);
+function searchCity(city) {
+  let apiKey = "bof6afef739a369a2ca570a747e3a1ct";
+  let apiUrl = 'https://api.shecodes.io/weather/v1/current?${apiUrl}=${apiKey}&units=metric';
+  axios.get(apiUrl).then(refreshWeather);
 }
-let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 searchCity("Polokwane");
